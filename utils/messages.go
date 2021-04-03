3f937515af561dbbeb1177f0b16ed0806a9c0256
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-04-04 04:03:27 +0900

    🌱 Add some fixed values

---
+++ utils/messages.go
@@ -0,0 +1,7 @@
+package utils
+
+const (
+	MessageNotFoundError   = "Specified content was not exist."
+	MessagePermissionError = "You don't have enough permission to do it"
+	MessageInternalError   = "Unfortunately, the server exploded"
+)
---
+++ utils/permissions.go
@@ -0,0 +1,7 @@
+package utils
+
+const (
+	PermissionAdmin     = 9
+	PermissionModelator = 5
+	PermissionUser      = 0
+)
