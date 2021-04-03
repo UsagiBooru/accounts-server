fe0f29f0c428188facc822cb7ec3d1f10dfe80f7
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-04-04 03:26:29 +0900

    🚧 Add methods to get headers

--- utils/load_header.go
+++ utils/load_header.go
@@ -2,23 +2,51 @@ package utils
 
 import (
 	"context"
-	"fmt"
+	"errors"
 	"net/http"
 
 	"github.com/UsagiBooru/accounts-server/gen"
 	"github.com/gorilla/mux"
 )
 
+type key int
+
+const context_user_id key = 1
+const context_user_permission key = 2
+
+// load permission from context
+func GetUserPermission(ctx context.Context) (int32, error) {
+	v := ctx.Value(context_user_permission)
+	permission, ok := v.(int32)
+	if !ok {
+		return 0, errors.New("could not parse permission header")
+	}
+	return permission, nil
+}
+
+// load user id from context
+func GetUserID(ctx context.Context) (int32, error) {
+	v := ctx.Value(context_user_id)
+	user_id, ok := v.(int32)
+	if !ok {
+		return 0, errors.New("could not parse user id header")
+	}
+	return user_id, nil
+}
+
+// middleware to set context
 func injectHeaderToContext(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("[START] middleware1")
-		ctx := context.WithValue(r.Context(), "user-id", "123")
+		user_id := r.Header.Get("x-consumer-user-id")
+		user_permission := r.Header.Get("x-consumer-user-permission")
+		ctx := context.WithValue(r.Context(), context_user_id, user_id)
+		ctx = context.WithValue(ctx, context_user_permission, user_permission)
 		r = r.WithContext(ctx)
 		next.ServeHTTP(w, r)
-		fmt.Println("[END] middleware1")
 	}
 }
 
+// router creater for use middleware
 func NewRouterWithInject(routers ...gen.Router) *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
 	for _, api := range routers {
